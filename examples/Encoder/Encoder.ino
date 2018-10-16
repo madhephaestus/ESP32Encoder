dabb9bf6075952d0f812551c34ad64312450904b
Kevin Harrington <mad.hephaestus@gmail.com>
2018-10-15 23:10:42 -0400

    Encoder library interrupt driven

--- examples/Encoder/Encoder.ino
+++ examples/Encoder/Encoder.ino
@@ -1,1 +1,16 @@
 
+#include <ESP32Encoder.h>
+ESP32Encoder encoder;
+ESP32Encoder encoder2;
+void setup(){
+	Serial.begin(115200);
+	Serial.println("Encoder Start = "+String((int32_t)encoder.getCount()));
+
+	encoder.attachFullQuad(36, 39);
+	encoder2.attachFullQuad(34, 35);
+}
+
+void loop(){
+	Serial.println("Encoder count = "+String((int32_t)encoder.getCount())+" "+String((int32_t)encoder2.getCount()));
+	delay(100);
+}
--- src/ESP32Encoder.h
+++ src/ESP32Encoder.h
@@ -1,1 +1,22 @@
+#pragma once
+#include <Arduino.h>
+#define MAX_ESP32_ENCODERS 10
+class ESP32Encoder {
+private:
+	void attach(int aPintNumber, int bPinNumber, boolean fullQuad);
+	portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+	boolean attached=false;
+	int aPinNumber;
+	int bPinNumber;
+	int64_t count=0;
 
+public:
+	ESP32Encoder();
+	~ESP32Encoder();
+	void attachFullQuad(int aPintNumber, int bPinNumber);
+	void attachHalfQuad(int aPintNumber, int bPinNumber);
+	int64_t getCount();
+	void interruptA();
+	void interruptB();
+
+};
