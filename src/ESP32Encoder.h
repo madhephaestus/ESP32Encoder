e95adbaa699f56ce79428ee0ac19e3d967925295
Kevin Harrington <mad.hephaestus@gmail.com>
2018-10-16 17:37:42 -0400

    Adding the static to the public members

--- src/ESP32Encoder.h
+++ src/ESP32Encoder.h
@@ -9,7 +9,6 @@ private:
 	int aPinNumber;
 	int bPinNumber;
 	int64_t count=0;
-	static ESP32Encoder *encoders[MAX_ESP32_ENCODERS];
 public:
 	ESP32Encoder();
 	~ESP32Encoder();
@@ -20,5 +19,7 @@ public:
 	void interruptB();
 	boolean isAttached(){return attached;}
 	void setCount(int64_t value);
+	static ESP32Encoder *encoders[MAX_ESP32_ENCODERS];
+
 
 };
