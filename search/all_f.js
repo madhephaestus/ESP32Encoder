03f5b20c1cc94160b8d0614bdd263f6d62f35b62
Kevin Harrington <mad.hephaestus@gmail.com>
2021-03-22 13:38:26 -0400

    updating the doxygen

--- search/all_f.js
+++ search/all_f.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['setcount',['setCount',['../classESP32Encoder.html#ace023e5b010634512ffef0e8ec23ae3e',1,'ESP32Encoder']]],
+  ['setfilter',['setFilter',['../classESP32Encoder.html#ae3cecb7d572685b3195f8a13409b3390',1,'ESP32Encoder']]],
   ['setup',['setup',['../Encoder_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'Encoder.ino']]],
   ['single',['single',['../ESP32Encoder_8h.html#a94b99e462b91a04d4400c30ce0764d60a44512857a6d1077e4462c754c0a1d11b',1,'ESP32Encoder.h']]]
 ];
