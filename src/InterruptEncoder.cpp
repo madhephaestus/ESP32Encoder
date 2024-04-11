34cb8ab292d913327c3e14f124523feb975918a8
Forairaaaaa <applesyqd@outlook.com>
2024-04-11 10:38:30 +0800

    add interrupt

--- src/InterruptEncoder.cpp
+++ src/InterruptEncoder.cpp
@@ -7,40 +7,99 @@
 #include "InterruptEncoder.h"
 
 #ifdef ARDUINO
-void IRAM_ATTR encoderAISR(void * arg) {
-	InterruptEncoder* object=(InterruptEncoder*)arg;
-	long start = micros();
-	long duration=start - object->microsLastA;
-	if (duration >= US_DEBOUNCE) {
-		object->microsLastA = start;
-		object->microsTimeBetweenTicks=duration;
-		object->aState = digitalRead(object->apin);
-		object->bState = digitalRead(object->bpin);
-		if (object->aState == object->bState)
-			object->count = object->count + 1;
-		else
-			object->count = object->count - 1;
-	}
+void IRAM_ATTR encoderAISR(void* arg)
+{
+    InterruptEncoder* object = (InterruptEncoder*)arg;
+    long start = micros();
+    long duration = start - object->microsLastA;
+    if (duration >= US_DEBOUNCE)
+    {
+        object->microsLastA = start;
+        object->microsTimeBetweenTicks = duration;
+        object->aState = digitalRead(object->apin);
+        object->bState = digitalRead(object->bpin);
+        if (object->aState == object->bState)
+            object->count = object->count + 1;
+        else
+            object->count = object->count - 1;
+    }
 }
 InterruptEncoder::InterruptEncoder() {}
-InterruptEncoder::~InterruptEncoder() {
-	if(attached)
-		detachInterrupt(digitalPinToInterrupt(apin));
+InterruptEncoder::~InterruptEncoder()
+{
+    if (attached)
+        detachInterrupt(digitalPinToInterrupt(apin));
 }
-int64_t  InterruptEncoder::read(){
-	return count*2;
+int64_t InterruptEncoder::read() { return count * 2; }
+void InterruptEncoder::attach(int aPinNum, int bPinNum)
+{
+    if (attached)
+        return;
+    apin = aPinNum;
+    bpin = bPinNum;
+    pinMode(apin, INPUT_PULLUP);
+    pinMode(bpin, INPUT_PULLUP);
+    attachInterruptArg(digitalPinToInterrupt(apin), encoderAISR, this, CHANGE);
 }
-void InterruptEncoder::attach(int aPinNum, int bPinNum) {
-	if(attached)
-		return;
-	apin = aPinNum;
-	bpin = bPinNum;
-	pinMode(apin, INPUT_PULLUP);
-	pinMode(bpin, INPUT_PULLUP);
-	attachInterruptArg(digitalPinToInterrupt(apin), encoderAISR,this, CHANGE);
-}
-#else 
-// TODO
+#else
+#include <freertos/FreeRTOS.h>
+#include <driver/gpio.h>
+#include <esp_timer.h>
 
+#define micros() esp_timer_get_time()
+#define digitalRead(pin) gpio_get_level((gpio_num_t)pin)
 
+static void IRAM_ATTR encoderAISR(void* arg)
+{
+    InterruptEncoder* object = (InterruptEncoder*)arg;
+    long start = micros();
+    long duration = start - object->microsLastA;
+    if (duration >= US_DEBOUNCE)
+    {
+        object->microsLastA = start;
+        object->microsTimeBetweenTicks = duration;
+        object->aState = digitalRead(object->apin);
+        object->bState = digitalRead(object->bpin);
+        if (object->aState == object->bState)
+            object->count = object->count + 1;
+        else
+            object->count = object->count - 1;
+    }
+}
+
+InterruptEncoder::InterruptEncoder() {}
+
+InterruptEncoder::~InterruptEncoder()
+{
+    // if(attached)
+    // 	detachInterrupt(digitalPinToInterrupt(apin));
+
+    if (attached)
+    {
+        gpio_isr_handler_remove((gpio_num_t)apin);
+        gpio_uninstall_isr_service();
+    }
+}
+
+int64_t InterruptEncoder::read() { return count * 2; }
+
+void InterruptEncoder::attach(int aPinNum, int bPinNum)
+{
+    if (attached)
+        return;
+    apin = aPinNum;
+    bpin = bPinNum;
+
+    // pinMode(apin, INPUT_PULLUP);
+    // pinMode(bpin, INPUT_PULLUP);
+    // attachInterruptArg(digitalPinToInterrupt(apin), encoderAISR,this, CHANGE);
+
+    gpio_reset_pin((gpio_num_t)apin);
+    gpio_set_direction((gpio_num_t)apin, GPIO_MODE_INPUT);
+    gpio_set_level((gpio_num_t)apin, GPIO_PULLUP_ONLY);
+    gpio_set_intr_type((gpio_num_t)apin, GPIO_INTR_ANYEDGE);
+
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add((gpio_num_t)apin, encoderAISR, (void*)this);
+}
 #endif
